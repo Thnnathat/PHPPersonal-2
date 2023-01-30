bdd80f195aac4c6e83795736ba4110e63bfae3e8
Thnnathat <thnnathatchaiphutha@gmail.com>
2023-01-30 11:11:38 +0700

    Last Commit

--- list_persons.php
+++ list_persons.php
@@ -12,18 +12,42 @@
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
 
+    $sql = "SELECT id, f_name, l_name, age, sex, married_status FROM survay";
+    $result = $conn->query($sql);
+
+
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
-    $sql = "SELECT * FROM sirway";
-    $result = $conn->query($sql);
-
     if ($result->num_rows > 0) {
         // output data of each row
         while ($row = $result->fetch_assoc()) {
-            echo "id: " . $row["id"] . " - Name: " . $row["f_name"] . " " . $row["l_name"] . "- Age: " . $row["age"] . " - Sex: " . $row["sex"] . " - Married Status: " . $row["married_status"] . "<br>";
+            $f_name = $row["f_name"];
+            $l_name = $row["l_name"];
+            $age = $row["age"];
+            $gender = $row["sex"];
+            $married = $row["married_status"];
+            $prefix = "";
+
+            if ($gender == "m") {
+                if ($age >= 15) {
+                    $prefix = "นาย";
+                } else {
+                    $prefix = "เด็กชาย";
+                }
+            } else {
+                if ($married == "m") {
+                    $prefix = "นาง";
+                } else {
+                    if ($age < 15) {
+                        $prefix = "เด็กหญิง";
+                    } else {
+                        $prefix = "นางสาว";
+                    }
+                }
+            }
+            echo "Name: " . $prefix . " " . $f_name." ".$l_name. "<br>";    
         }
     } else {
         echo "0 results";
